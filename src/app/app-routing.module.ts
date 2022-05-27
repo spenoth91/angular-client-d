ab8ceba67b95d1bfdd284a75ff14b878b3e5b1b6
root <sergiu_claudiu7@yahoo.com>
2022-05-27 14:08:55 +0300

    booking folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { UserListComponent } from "./users/components/user-list/user-list.component";
-import { BookingListComponent } from "./users/components/booking-list/booking-list.component";
+import { BookingListComponent } from "./bookings/components/booking-list/booking-list.component";
 
 const routes: Routes = [
   { path: "users", component: UserListComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
-import {TableModule} from 'primeng/table';
-import {UserListComponent} from './users/components/user-list/user-list.component';
-import { BookingListComponent } from './users/components/booking-list/booking-list.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { TableModule } from "primeng/table";
+import { UserListComponent } from "./users/components/user-list/user-list.component";
+import { BookingListComponent } from "./bookings/components/booking-list/booking-list.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UserListComponent,
-    BookingListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    TableModule
-  ],
+  declarations: [AppComponent, UserListComponent, BookingListComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, TableModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
