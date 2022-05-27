f90eb4a90d8528247183a6d67f3677d3155afe54
Raul Pop <popraul17@gmail.com>
2022-05-27 13:15:17 +0300

    booking service, model, component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { UserListComponent } from "./users/components/user-list/user-list.component";
 import { BookingListComponent } from "./users/components/booking-list/booking-list.component";
+import {PropertyListComponent} from "./users/components/property-list/property-list.component";
 
 const routes: Routes = [
   { path: "users", component: UserListComponent },
   { path: "bookings", component: BookingListComponent },
+  { path: "property", component: PropertyListComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import {HttpClientModule} from '@angular/common/http';
 import {TableModule} from 'primeng/table';
 import {UserListComponent} from './users/components/user-list/user-list.component';
 import { BookingListComponent } from './users/components/booking-list/booking-list.component';
+import {PropertyListComponent} from "./users/components/property-list/property-list.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
+    PropertyListComponent,
     BookingListComponent
   ],
   imports: [
